18e958d47dc5a23ace6e249f6f6a1ac94c54f4d5
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2024-01-31 21:26:37 +0200

    initial commit of 404 page

---
+++ wp-content/themes/softuni/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+        <!-- Full Page Image Header Area -->
+        <div id="top" class='not-found'>
+            <div class="vert-text parallax">
+                <h1 class="header-text"><?php _e( '404, PAGE NOT FOUND', 'softuni' ); ?> </h1>
+            </div>
+        </div>
+        
+    <?php get_footer(); ?>
