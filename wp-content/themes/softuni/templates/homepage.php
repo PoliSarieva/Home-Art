8298e163494d7ea629ddb90324e065dc238709e8
PoliSarieva <109367470+PoliSarieva@users.noreply.github.com>
2024-02-04 11:59:40 +0200

    Create homepage template

---
+++ wp-content/themes/softuni/templates/homepage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Homepage
+ */
+?>
+
+<?php get_header(); ?>
+        <!-- Full Page Image Header Area -->
+        <div id="top" class="header">
+            <div class="vert-text parallax">
+                <h1 class="header-text">HOME ART</h1>
+                <h3 class="header-text">
+                    <em class="header-text">So we are here to guide you</em>
+                </h3><br />
+                <a href="#about" class="btn header-btn">Find Out More</a>
+                <br/>
+                <a href="#about"><img src="http://localhost/softuni/wp-content/themes/softuni/img/direction.png" class="space" alt="Direction"></a>
+            </div>
+        </div>
+        
+    <?php get_footer(); ?>
